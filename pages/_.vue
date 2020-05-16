00d55ba71cc79a79c257522875702b9bf46dde62
clin <39868400+clinfc@users.noreply.github.com>
2020-05-16 17:36:22 +0800

    error页面

---
+++ pages/_.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="error">
+    404
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'error'
+}
+</script>
+
+<style>
+.error {
+  font-size: 100px;
+  font-weight: bold;
+}
+</style>
